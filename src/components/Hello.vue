dd96ea013dc93034140dc374ec75f180e58234ad
595806119@qq.com <595806119@qq.com>
2017-08-02 17:39:30 +0800

    搭建开发环境

---
+++ src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+  <Button>ASD</Button>
+  <Cell>CVB</Cell>
+  </div>
+</template>
+
+<script>
+import { Button, Cell } from 'mint-ui';
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    Button,
+    Cell
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
